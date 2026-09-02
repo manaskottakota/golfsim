169fdfd063458e12279378647eecd606e53321a4
manaskottakota <manaskottakota@gmail.com>
2026-09-02 16:23:42 +0530

    Initial Commit

---
+++ golfsim/golfsimApp.swift
@@ -0,0 +1,17 @@
+//
+//  golfsimApp.swift
+//  golfsim
+//
+//  Created by Manas Kottakota on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct golfsimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
